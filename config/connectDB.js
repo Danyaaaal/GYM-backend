d52174e60bc159a0875afc0fd424d0eafe176c48
Dany <danialikhani@gmail.com>
2024-04-25 15:33:29 +0200

    start from scratch

---
+++ config/connectDB.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import colors from "colors";
+
+const { MONGODB_URI } = process.env;
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(MONGODB_URI);
+    console.log(`::`.grey, `MongoDB connected: ${conn.connection.host}`.yellow);
+  } catch (error) {
+    console.error(`Could not connect to MongoDB: ${error.message}`);
+    process.exit(1); // exit the process with failure
+  }
+};
+
+export default connectDB;
--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-import express from "express"
- const {PORT} = process.env
- const app = express()
+import express from "express";
+import connectDB from "./config/connectDB.js";
 
+const { PORT } = process.env;
 
+// EXPRESS SERVER INSTANCE
+const app = express();
 
- app.listen(PORT,(req,res) => {
-    console.log(`server is running on http://localhost:${PORT}`)
- })
+// DB CONNECTION
+await connectDB();
+
+// URL ENCODED MIDDLEWARE
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/",(req,res)=>{
+   res.send("hello")
+});
+
+app.listen(PORT, (req, res) => {
+  console.log(`server is running on http://localhost:${PORT}`);
+});
